39283df38d2ceed851d3154fccdbec6ee5253f55
SPOOK_EXE <91944990+SPOOKEXE@users.noreply.github.com>
2023-09-19 14:39:03 +0930

    working-state

---
+++ download.py
@@ -0,0 +1,55 @@
+
+import instagrapi
+
+from os import makedirs
+
+def get_credentials( ) -> tuple:
+	try:
+		with open("credentials", "r") as file:
+			return tuple( file.readlines() )
+	except Exception as exception:
+		print("Failed to read credentials file.")
+		print( exception )
+	return (None, None)
+
+user, passwrd = get_credentials()
+
+cl = instagrapi.Client()
+
+print("Logging into account.")
+assert cl.login(user, passwrd), "Failed to log into the account."
+print("Logged in.")
+
+print("Scanning post collection - all items.")
+try:
+	collection = cl.collection_medias('All Posts', amount=9999)
+except Exception as e:
+	print("Failed to get all posts - reason: ")
+	print(e)
+
+print("Downloading found media.")
+for item in [ "photos", "videos", "album" ]:
+	makedirs(item, exist_ok=True)
+
+def download_media_item( item ) -> None:
+	if item.media_type == 1:
+		# photo
+		cl.photo_download(item.pk, "photos")
+	elif item.media_type == 2:
+		# video
+		cl.video_download(item.pk, "videos")
+	elif item.media_type == 8:
+		# album
+		cl.album_download( item.pk, "album" )
+
+print("Appending downloads to pool: ", len( collection ))
+
+for index, item in enumerate(collection):
+	try:
+		print(index, ' / ', len(collection), ' - ', item.pk)
+		download_media_item( item )
+	except:
+		with open("failed.txt", "a") as file:
+			file.write(f"{item.pk} {item.media_type}\n")
+
+print("Downloaded all media.")
---
+++ download_failed.py
@@ -0,0 +1,46 @@
+import instagrapi
+
+from os import makedirs
+from concurrent import futures
+
+with open("failed.txt", "r") as file:
+	lines = [ line for line in file.readlines() if line.strip() != "" ]
+
+def get_credentials( ) -> tuple:
+	try:
+		with open("credentials", "r") as file:
+			return tuple( file.readlines() )
+	except Exception as exception:
+		print("Failed to read credentials file.")
+		print( exception )
+	return (None, None)
+
+user, passwrd = get_credentials()
+
+cl = instagrapi.Client()
+
+def download_media_item( pk, media_type ) -> None:
+	if item.media_type == 1:
+		# photo
+		cl.photo_download(item.pk, "photos")
+	elif item.media_type == 2:
+		# video
+		cl.video_download(item.pk, "videos")
+	elif item.media_type == 8:
+		cl.album_download( item.pk, "album" )
+
+for item in [ "photos", "album", "videos" ]:
+	makedirs(item, exist_ok=True)
+
+pool = futures.ThreadPoolExecutor(max_workers=12)
+
+print("Appending downloads to pool: ", len( lines ))
+items = [ ]
+for index, line in enumerate(lines):
+	pk, media_type = tuple( line.split(" ") )
+	print(index, pk)
+	try:
+		download_media_item( pk, media_type )
+	except:
+		with open("failed_again.txt", "a") as file:
+			file.write(f"{pk} {media_type}\n")
